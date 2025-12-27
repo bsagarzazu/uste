cf42e38f0885fe206460fbfe1af50d5884fc01c2
Beñat Sagarzazu <144722765+bsagarzazu@users.noreply.github.com>
2025-12-27 14:50:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
   }
 });
